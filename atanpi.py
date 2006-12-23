6236db7cf551ba83bbe954d7ff0d0b5964fbd6f8
richard <richard@a1ed64d9-fc25-0410-9a17-eb38f75f73fa>
2006-12-23 21:54:51 +0000

    changed from pi/6 to pi/8 series

--- atanpi.py
+++ atanpi.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # simple approximation to pi, based on atan(1/sqrt(3))
+# $Revision$, $Date$
 # RW Penney, December 2006
 
 import math
@@ -7,7 +8,7 @@ import math
 terms = 15
 
 
-def main():
+def six():
     scl = 2 * math.sqrt(3)
     sum = 1.0
     fac = 4.0 / 9.0
@@ -16,8 +17,25 @@ def main():
         sum -= fac * (2 * idx + 1.0) / (16 * idx * idx - 1.0)
         fac /= 9.0
         idx += 1
-        print sum * scl
+        print sum * scl, '   ', math.sin(sum * scl)
 
+def eight():
+    rt2 = math.sqrt(2)
+    scl = 8 * math.sqrt((rt2 - 1) / (rt2 + 1))
+    sum = 1.0
+    fac = 1.0
+    idx = 1
+    while idx < terms:
+        fac *= (3 - 2 * rt2) / (3 + 2 * rt2)
+        sum -= fac * (8 * idx + 2 * rt2) /   \
+                ((rt2 - 1.0) * (16 * idx * idx - 1.0))
+        idx += 1
+        print sum * scl, '   ', math.sin(sum * scl)
+
+def main():
+    six()
+    print
+    eight()
 
 if __name__ == "__main__":
     main()
