49f1b11ab18defbf78648334be7bdc2d18dd4c9c
richard <richard@a1ed64d9-fc25-0410-9a17-eb38f75f73fa>
2006-12-25 08:24:22 +0000

    improved starting approximation for sqrt iteration added exp & sincos methods

--- FixedPoint.py
+++ FixedPoint.py
@@ -13,6 +13,7 @@ class FPnum(object):
         self.value = long(val * self.scale)
 
     def SetFraction(self, n_bits=32):
+        # change number of fractional bits (call before creating numbers!)
         self.fraction_bits = n_bits
         scale = 1L << n_bits
         round = 1L << (n_bits - 1)
@@ -97,12 +98,51 @@ class FPnum(object):
 
     # mathematical functions:
     def sqrt(self):
-        rt = FPnum(1)
+        # calculate crude initial approximation:
+        rt = FPnum()
+        rt.value = 1L << (self.fraction_bits / 2)
+        val = self.value
+        while val > 0:
+            val >>= 2
+            rt.value <<= 1
+        # refine approximation by Newton iteration:
         while True:
             delta = (rt - self / rt) / FPnum(2)
             rt -= delta
             if delta.value == 0: break
         return rt
+
+    def exp(self):
+        ex = FPnum(1)
+        term = FPnum(1)
+        idx = FPnum(1)
+        while True:
+            term *= self / idx
+            ex += term
+            idx += FPnum(1)
+            if term.value == 0: break
+        return ex
+
+    def sincos(self):
+        sn = FPnum(0)
+        cs = FPnum(1)
+        term = FPnum(1)
+        idx = 1
+        while (True):
+            term *= self / FPnum(idx)
+            if (idx % 2) == 0:
+                if (idx % 4) == 0:
+                    cs += term
+                else:
+                    cs -= term
+            else:
+                if (idx % 4) == 1:
+                    sn += term
+                else:
+                    sn -= term
+            idx += 1
+            if term.value == 0: break
+        return (sn, cs)
 
 
 if __name__ == "__main__":
@@ -110,6 +150,8 @@ if __name__ == "__main__":
     for idx in xrange(0,32):
         print q
         q /= FPnum(2)
-    rt = FPnum(2).sqrt()
-    print rt
-    print rt * rt
+    val = 2
+    rt = FPnum(val).sqrt()
+    print 'sqrt(' + str(val) + ') = ', rt
+    print '  val ~ ', rt * rt
+    print 'exp(1) = ', FPnum(1).exp()
